27a29d47bea86bb752eed3938af3e90d9fec0709
Alex Lee <58710274+aklee2106@users.noreply.github.com>
2024-07-01 17:58:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import myFirstApp from './my-first-app';
-import myFirstAdvancedKviewApp from './my-first-advanced-kview-app';
-import myFirstWidgetApp from './my-first-widget-app';
-import myFirstOutboundWebhookApp from './my-first-outbound-webhook-app';
+import tutorialApp from './giphy_tutorial_zzz-alex-allene';
 
 const apps = [
-    myFirstApp,
-    myFirstWidgetApp,
-    myFirstAdvancedKviewApp,
-    myFirstOutboundWebhookApp,
+    tutorialApp
 ];
 
 export { apps as default };
